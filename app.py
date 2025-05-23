ac910850ecafd82d1cfc9326f161fb7f3c49ae45
villelabruno <bruno.villela1991@icloud.com>
2025-05-23 17:25:14 -0300

    primeiro commit do projeto Flask

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    usuario = None
+    tarefas = []
+    mostrar_boas_vindas = False
+
+    if request.method == "POST":
+        usuario = request.form.get("nome")
+        tarefas = ["Estudar Flask", "Fazer exercício", "Ler livro"]
+        mostrar_boas_vindas = True
+    return render_template("home.html", usuario=usuario, tarefas=tarefas, mostrar=mostrar_boas_vindas)
+
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+
+if __name__ == "__main__":
+    app.run
+    # (debug=True, host="0.0.0.0")
